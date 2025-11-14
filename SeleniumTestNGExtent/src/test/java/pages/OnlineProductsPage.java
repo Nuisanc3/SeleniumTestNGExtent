46e5c2f1d96b83b32c097776dcbbfd3b067e2f75
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-14 22:37:34 +0530

    Added a few methods and fixed errors

--- SeleniumTestNGExtent/src/test/java/pages/OnlineProductsPage.java
+++ SeleniumTestNGExtent/src/test/java/pages/OnlineProductsPage.java
@@ -1,28 +1,38 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import static Utilities.GetUtility.getText;
+import static pages.HomePage.find;
 
 public class OnlineProductsPage {
 
-    private By formalShoesTitle = By.xpath("//h2[@class='FormalShoesTitle']");
-    private By sportsShoesTitle = By.xpath("//h2[@class='SportsShoesTitle']");
-    private By SneakerShoesTitle = By.xpath("//h2[@class='SneakerShoesTitle']");
+    private static By formalShoesTitle = By.xpath("//h2[@class='FormalShoesTitle']");
+    private static By sportsShoesTitle = By.xpath("//h2[@class='SportsShoesTitle']");
+    private static By SneakerShoesTitle = By.xpath("//h2[@class='SneakerShoesTitle']");
     private By formalShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down formalshoedropdown']");
     private By sportsShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down sportsshoedropdown']");
     private By sneakerShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down sneakershoedropdown']");
     private By formalShoesFirstShoe = By.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]");
     private By sportShoesFirstShoe = By.xpath("/html/body/div[3]/table/tbody/tr[1]/td[1]");
 
-    public static String getFormalShoesTitleText() {
-    return "";
+    public static void getFormalShoesTitleText() {
+    String actualTitleFS =  getText(formalShoesTitle);
+    String expectedTitleFS = "Formal Shoes";
+        Assert.assertEquals(actualTitleFS, expectedTitleFS);
     }
 
-    public static String getsportsShoesTitleText() {
-        return "";
+    public static void getsportsShoesTitleText() {
+        String actualTitleSS =  getText(sportsShoesTitle);
+        String expectedTitleSS = "Sports Shoes";
+        Assert.assertEquals(actualTitleSS, expectedTitleSS);
     }
 
-    public static String getSneakerShoesTitleText() {
-        return "";
+    public static void getSneakerShoesTitleText() {
+        String actualTitleSnS =  getText(SneakerShoesTitle);
+        String expectedTitleSnS = "Sneaker Shoes";
+        Assert.assertEquals(actualTitleSnS, expectedTitleSnS);
     }
 
 
