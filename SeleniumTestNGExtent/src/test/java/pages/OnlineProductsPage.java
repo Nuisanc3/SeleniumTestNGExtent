7772d2efb733671ed73c084f6ac6329b2775a16a
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-14 23:01:52 +0530

    Completed both tests required for this project

--- SeleniumTestNGExtent/src/test/java/pages/OnlineProductsPage.java
+++ SeleniumTestNGExtent/src/test/java/pages/OnlineProductsPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 
 import static Utilities.GetUtility.getText;
+import static pages.HomePage.click;
 import static pages.HomePage.find;
 
 public class OnlineProductsPage {
@@ -11,48 +12,55 @@ public class OnlineProductsPage {
     private static By formalShoesTitle = By.xpath("//h2[@class='FormalShoesTitle']");
     private static By sportsShoesTitle = By.xpath("//h2[@class='SportsShoesTitle']");
     private static By SneakerShoesTitle = By.xpath("//h2[@class='SneakerShoesTitle']");
-    private By formalShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down formalshoedropdown']");
-    private By sportsShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down sportsshoedropdown']");
-    private By sneakerShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down sneakershoedropdown']");
-    private By formalShoesFirstShoe = By.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]");
-    private By sportShoesFirstShoe = By.xpath("/html/body/div[3]/table/tbody/tr[1]/td[1]");
+    private static By formalShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down formalshoedropdown']");
+    private static By sportsShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down sportsshoedropdown']");
+    private static By sneakerShoesDropdown = By.xpath("//i[@class='fa fa-angle-double-down sneakershoedropdown']");
+    private static By formalShoesFirstShoe = By.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]");
+    private static By sportShoesFirstShoe = By.xpath("/html/body/div[3]/table/tbody/tr[1]/td[1]");
 
-    public static void getFormalShoesTitleText() {
+    public static void getFormalShoesTitleText_Verify() {
     String actualTitleFS =  getText(formalShoesTitle);
     String expectedTitleFS = "Formal Shoes";
         Assert.assertEquals(actualTitleFS, expectedTitleFS);
     }
 
-    public static void getsportsShoesTitleText() {
+    public static void getsportsShoesTitleText_Verify() {
         String actualTitleSS =  getText(sportsShoesTitle);
         String expectedTitleSS = "Sports Shoes";
         Assert.assertEquals(actualTitleSS, expectedTitleSS);
     }
 
-    public static void getSneakerShoesTitleText() {
+    public static void getSneakerShoesTitleText_Verify() {
         String actualTitleSnS =  getText(SneakerShoesTitle);
-        String expectedTitleSnS = "Sneaker Shoes";
+        String expectedTitleSnS = "Sneakers";
         Assert.assertEquals(actualTitleSnS, expectedTitleSnS);
     }
 
 
     public static void clickFormalShoesDropdown() {
+        click(formalShoesDropdown);
+
 
     }
 
     public static void clickSportsShoesDropdown() {
+        click(sportsShoesDropdown);
+
     }
 
     public static void clickSneakerShoesDropdown() {
     }
 
-    public static String getFirstFormalShoesTitle() {
-        return "";
-
+    public static void getFirstFormalShoesTitle_Verify() {
+        String actualFirstShoeFS = getText(formalShoesFirstShoe);
+        String expectedFirstShoeFS = "   Classic Cheltenham";
+        Assert.assertEquals(actualFirstShoeFS, expectedFirstShoeFS);
     }
 
-    public static String getFirstSportShoesTitle() {
-        return "";
+    public static void getFirstSportShoesTitle_Verify() {
+        String actualFirstShoeFS = getText(sportShoesFirstShoe);
+        String expectedFirstShoeFS = "   Ultimate";
+        Assert.assertEquals(actualFirstShoeFS, expectedFirstShoeFS);
 
     }
 
--- SeleniumTestNGExtent/src/test/java/testcases/BaseTest.java
+++ SeleniumTestNGExtent/src/test/java/testcases/BaseTest.java
@@ -1,27 +1,47 @@
 package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.OnlineProductsPage;
 
 import static pages.HomePage.driver;
 
 public class BaseTest {
 
     @BeforeTest
-    public static void setUp() {
+    public static void setUp() throws InterruptedException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+        HomePage.clickHamburgerMenuButton();
+        HomePage.clickOnlineProductsMenuItem();
 
     }
 
     @Test
     public static void validateTitlesOnTheOnlineProductsPage() throws InterruptedException {
-        HomePage.clickHamburgerMenuButton();
-        HomePage.clickOnlineProductsMenuItem();
+        OnlineProductsPage.getFormalShoesTitleText_Verify();
+        OnlineProductsPage.getsportsShoesTitleText_Verify();
+        OnlineProductsPage.getSneakerShoesTitleText_Verify();
+    }
 
+    @Test
+    public static void validateFirstFormalShoes() {
+        OnlineProductsPage.clickFormalShoesDropdown();
+        OnlineProductsPage.getFirstFormalShoesTitle_Verify();
+    }
 
+    @Test
+    public static void validateFirstSportsShoes() {
+        OnlineProductsPage.clickSportsShoesDropdown();
+        OnlineProductsPage.getFirstSportShoesTitle_Verify();
+    }
+
+    @AfterTest
+    public static void tearDown() {
+        driver.quit();
     }
 }
